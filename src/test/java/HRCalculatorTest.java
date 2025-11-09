e30ebcdb8c5fc23b79ae27d4ab4b2066f84e987e
Julia Garbera <278443@student.pwr.edu.pl>
2025-11-09 16:34:19 +0100

    Initial project setup with Maven and HRCalculator

---
+++ src/test/java/HRCalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import pl.julia.HRCalculator;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HRCalculatorTest {
+    @Test
+    public void testValidMaxHR() {
+        var result = HRCalculator.calculateMaxHR(25);
+        assertEquals(195, result);
+    }
+}
