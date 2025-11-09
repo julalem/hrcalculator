5b97c661ea882c40c234e773402a62821c6508c2
Julia Garbera <278443@student.pwr.edu.pl>
2025-11-09 17:22:02 +0100

    Fix: Add check for negative age

--- src/main/java/pl/julia/HRCalculator.java
+++ src/main/java/pl/julia/HRCalculator.java
@@ -2,7 +2,7 @@ package pl.julia;
 
 public class HRCalculator {
     public static int calculateMaxHR(int age){
-        if(age>120){
+        if (age > 120 || age < 0) {
             throw new IllegalArgumentException(
                     "Age out of bounds"
             );
